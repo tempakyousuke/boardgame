8d5dc0dc10dce37ffed395a700fd35f59c31fd7d
tempakyousuke <15189821+tempakyousuke@users.noreply.github.com>
2025-05-31 15:28:00 +0900

    first commit

---
+++ src/lib/server/db/index.ts
@@ -0,0 +1,10 @@
+import * as schema from './schema';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import { env } from '$env/dynamic/private';
+
+if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+
+const client = postgres(env.DATABASE_URL);
+
+export const db = drizzle(client);

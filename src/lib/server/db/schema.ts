753c34da0ac8844c0162b7062ad7359c944873da
tempakyousuke <15189821+tempakyousuke@users.noreply.github.com>
2026-01-07 23:39:47 +0900

    remove age column

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -2,7 +2,6 @@ import { pgTable, text, integer, timestamp, boolean } from 'drizzle-orm/pg-core'
 
 export const user = pgTable('user', {
 	id: text('id').primaryKey(),
-	age: integer('age'),
 	username: text('username').notNull().unique(),
 	passwordHash: text('password_hash').notNull(),
 	displayName: text('display_name').notNull(),
